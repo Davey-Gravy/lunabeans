e6ced3cf837c9c1f2c258f020d396a04b4c7f733
Davis Cole <daveygravy17@gmail.com>
2024-04-28 16:31:12 -0400

    fix other things

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 mongo_uri = "mongodb://root:password@localhost:27778/?directConnection=true"
-db_name = "sample_mflix"
-coll_name = "movies"
+db_name = "test"
+coll_name = "test"
--- mongo.py
+++ mongo.py
@@ -3,3 +3,5 @@ import pymongo
 client = pymongo.MongoClient(
     "mongodb://root:password@localhost:27778/?directConnection=true"
 )
+collection = client['test']
+print(collection)
