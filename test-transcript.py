48c2041ea52eca4ab15b7119a38be0320891b159
bhoh <bhoh@mit.edu>
2024-04-28 15:08:50 -0400

    video transcription to single string

--- test-transcript.py
+++ test-transcript.py
@@ -1,12 +1,33 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from serach import search_videos
 
 def getTranscript(video_id):
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    return transcript
-
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        if transcript is None:
+            return None
+        else:
+            returnText = []
+            for item in transcript:
+                returnText.append(item['text'])
+            fullText = ' '.join(returnText)
+            return fullText
+    except Exception:
+        return None
 
 if __name__ == "__main__":
-    video_id = 'haT7uxmIFA8'
-    transcript = getTranscript(video_id)
-    for item in transcript:
-        print(item['text'])
+    search_term = 'python programming'
+    results = search_videos(search_term, 5)
+    for item in results['items']:
+        itemKind = item['id']['kind']
+        if itemKind == "youtube#video":
+            print(item['id']['videoId'])
+        elif itemKind == 'youtube#playlist':
+            print(item['id']['playlistId'])
+        
+    videoID = 'TOe_TvQLAA'
+    videoID1 = 'Wb3UrJjAac4'
+    videoID2 = 'Wb3UrJjAac4'
+    tr = getTranscript(videoID1)
+    print(tr)
+
